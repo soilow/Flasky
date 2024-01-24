31c06fb302382e151741add8f6daeb08d32b3be4
Dmitry Zmerlov <zmerzlovd@gmail.com>
2024-01-24 16:54:49 +0300

    Changes

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, redirect, abort, render_template, url_for
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -14,7 +18,7 @@ def internal_server_error(e):
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	return render_template("index.html", current_time=datetime.utcnow())
 
 @app.route("/user/<name>")
 def user(name):
