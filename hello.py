c549bbf635eccbf5dd6e3072ed86ca35832fba4f
Dmitry Zmerlov <zmerzlovd@gmail.com>
2024-01-25 19:17:56 +0300

    Changes

--- hello.py
+++ hello.py
@@ -4,18 +4,48 @@ from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 
+import os
 from datetime import datetime
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "adidas++"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class Role(db.Model):
+	__tablename__ = "roles"
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	users = db.relationship("User", backref='role', lazy="dynamic")
+
+	def __repr__(self):
+		return "<Role %r>" % self.name
+
+class User(db.Model):
+	__tablename__ = "users"
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+
+	def __repr__(self):
+		return "<User %r>" % self.username
 
 class NameForm(FlaskForm):
 	name = StringField("What is your name?", validators=[DataRequired()])
 	submit = SubmitField("Submit")
 
+
+@app.shell_context_processor
+def make_shell_context():
+	return dict(db=db, User=User, Role=Role)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -29,14 +59,24 @@ def internal_server_error(e):
 def index():
 	form = NameForm()
 	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash("Looks like you have changed your name!")
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username=form.name.data)
+			db.session.add(user)
+			db.session.commit()
+			session['known'] = False
+		else:
+			session['known'] = True
 
 		session['name'] = form.name.data
+		form.name.data = ""
 		return redirect(url_for('index'))
 
-	return render_template("index.html", form=form, name=session.get("name"), current_time=datetime.utcnow())
+	return render_template("index.html", 
+		form=form, 
+		name=session.get("name"), 
+		current_time=datetime.utcnow(),
+		known=session.get("known", False))
 
 @app.route("/user/<name>")
 def user(name):
